69e6faaf729aedd0634fee1c668f1e4f3e02a691
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-07-27 23:18:43 +0530

    links are set according to desktop screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router";
 
 const Navbar = () => {
   const links = [
     { link: "Home", to: "/" },
-    { link: "Home", to: "/" },
-    { link: "Home", to: "/" },
-    { link: "Home", to: "/" },
+    { link: "Research", to: "/" },
+    { link: "Team", to: "/" },
+    { link: "Dissertations", to: "/" },
+    { link: "Press", to: "/" },
+    { link: "Awards", to: "/" },
   ];
 
   return (
@@ -14,10 +17,17 @@ const Navbar = () => {
       <h1 className="text-lg md:text-xl font-semibold md:font-bold">
         Klinische Funktionelle Bildgebung
       </h1>
-      <div className="hidden md:flex links  items-center">
+      <div className="hidden md:flex links items-center gap-8">
+      <div className="flex links items-center gap-8">
         {links.map((link, idx)=>{
-                 
+          return (
+            <Link to={link.to} key={idx} className="">
+              {link.link}
+            </Link>
+          );
         })}
+      </div>
+      <button className="text-sm px-3 py-2 rounded-md font-semibold bg-[#F0F2F5] text-gray-600">DE | EN</button>
       </div>
       <RxHamburgerMenu className="md:hidden  text-lg" />
     </div>
