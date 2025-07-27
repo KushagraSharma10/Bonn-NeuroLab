8d12519e74ee0c1fde68b763c13a480da4b7a940
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-07-27 23:35:37 +0530

    update navbar links for correct routing and improve mobile menu accessibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,33 +5,34 @@ import { Link } from "react-router";
 const Navbar = () => {
   const links = [
     { link: "Home", to: "/" },
-    { link: "Research", to: "/" },
-    { link: "Team", to: "/" },
-    { link: "Dissertations", to: "/" },
-    { link: "Press", to: "/" },
-    { link: "Awards", to: "/" },
+    { link: "Research", to: "/research" },
+    { link: "Team", to: "/team" },
+    { link: "Dissertations", to: "/dissertations" },
+    { link: "Press", to: "/press" },
+    { link: "Awards", to: "/awards" },
   ];
 
   return (
-    <div className="flex items-center justify-between px-3 py-4 md:px-8 border-b-[1px] border-gray-200">
+    <div className="flex items-center justify-between px-3 py-4 md:px-8 border-b border-gray-200">
       <h1 className="text-lg md:text-xl font-semibold md:font-bold">
         Klinische Funktionelle Bildgebung
       </h1>
-      <div className="hidden md:flex links items-center gap-8">
-        <div className="flex links items-center gap-8">
-          {links.map((link, idx) => {
-            return (
-              <Link to={link.to} key={idx} className="">
-                {link.link}
-              </Link>
-            );
-          })}
-        </div>
-        <button className="text-sm px-3 py-2 rounded-md font-semibold bg-[#F0F2F5] text-gray-600 hover:cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out">
+
+      <nav className="hidden md:flex items-center gap-8">
+        {links.map(({ link, to }, idx) => (
+          <Link to={to} key={idx} className="">
+            {link}
+          </Link>
+        ))}
+
+        <button className="text-sm px-3 py-2 rounded-md font-semibold bg-[#F0F2F5] text-gray-600 hover:cursor-pointer hover:bg-gray-200 transition-all duration-300 ease-in-out">
           DE | EN
         </button>
-      </div>
-      <RxHamburgerMenu className="md:hidden  text-lg" />
+      </nav>
+
+      <button aria-label="Open menu" className="md:hidden">
+        <RxHamburgerMenu className="text-lg" />
+      </button>
     </div>
   );
 };
