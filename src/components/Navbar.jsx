100ec3cd4f525f666cc724c5e2890bb900c203a4
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-07-27 23:05:42 +0530

    react router is added and implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
+  const links = [
+    { link: "Home", to: "/" },
+    { link: "Home", to: "/" },
+    { link: "Home", to: "/" },
+    { link: "Home", to: "/" },
+  ];
+
   return (
-    <div className="flex items-center justify-between px-3 py-4 border-b">
-      <h1 className="text-lg font-semibold">
+    <div className="flex items-center justify-between px-3 py-4 md:px-8 border-b-[1px] border-gray-200">
+      <h1 className="text-lg md:text-xl font-semibold md:font-bold">
         Klinische Funktionelle Bildgebung
       </h1>
       <div className="hidden md:flex links  items-center">
-        
+        {links.map((link, idx)=>{
+                 
+        })}
       </div>
-      <RxHamburgerMenu className="text-lg" />
+      <RxHamburgerMenu className="md:hidden  text-lg" />
     </div>
   );
 };
